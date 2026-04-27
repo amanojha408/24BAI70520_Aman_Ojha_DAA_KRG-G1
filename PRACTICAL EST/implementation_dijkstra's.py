686db592ce22d1783b0c10664e7f09c010b9a2d4
Aman Raj Ojha <ojhaaman765@gmail.com>
2026-04-27 14:10:01 +0530

    Create implementation_dijkstra's.py

---
+++ PRACTICAL EST/implementation_dijkstra's.py
@@ -0,0 +1,26 @@
+import heapq
+
+class Solution:
+    def dijkstra(self, V, edges, src):
+        adj = [[] for _ in range(V)]
+        for u, v, w in edges:
+            adj[u].append((v, w))
+            adj[v].append((u, w))
+
+        dist = [float('inf')] * V
+        dist[src] = 0
+
+        pq = [(0, src)]
+
+        while pq:
+            d, u = heapq.heappop(pq)
+
+            if d > dist[u]:
+                continue
+
+            for v, w in adj[u]:
+                if dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+                    heapq.heappush(pq, (dist[v], v))
+
+        return dist
